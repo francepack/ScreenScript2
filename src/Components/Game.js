3d2559754f815c9b7347ce83e986cdf0318a4aac
Mason France <masonrf1@gmail.com>
2020-06-23 10:30:08 -0600

    Import components to Game component and create their divs

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Images from './Images';
+import Screenshare from './Screenshare';
+import Script from './Script';
+import ScriptSelector from './ScriptSelector';
 
 function Game() {
   return (
     <div className="game">
-      
+      <div className="screenshare-container">
+        <Screenshare />
+      </div>
+      <div className="images-container">
+        <Images />
+      </div>
+      <div className="script-container">
+        <Script />
+      </div>
+      <div className="scriptselector-container">
+        <ScriptSelector />
+      </div>
     </div>
   );
 }
