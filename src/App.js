177d2685818434bafcb7210345bf1ed3dacb2b52
Mason France <masonrf1@gmail.com>
2020-06-23 13:03:04 -0600

    Make App a container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import gameSets from '../public/assets/sampledata.js';
 import Header from './Components/Header'
 import Home from './Components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: "",
+      isLoading: false,
+      currentGameId: "",
+      isLoggedIn: false,
+      currentGroupId: "",
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
