c772751aff7473ea3568095991c29dffa3161e69
Mason France <masonrf1@gmail.com>
2020-06-23 10:02:16 -0600

    Add Home component template

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="game-selector">
+        <h2>Welcome!</h2>
+        <p>Choose your game:</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
