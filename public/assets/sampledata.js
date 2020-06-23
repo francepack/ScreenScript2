b07cd73c55e6d004970eaa8a98d998bf892f071d
Mason France <masonrf1@gmail.com>
2020-06-23 11:14:57 -0600

    Add template for sample data

---
+++ public/assets/sampledata.js
@@ -0,0 +1,215 @@
+var gameSets = [
+  {
+    name: "Awesome Games Inc",
+    logo: "logo_url",
+    background: "company_specific_img_url",
+    games: [
+      {
+        name: "Dungeon Crawl",
+
+      },
+      {
+        name: "Diving Adventure",
+
+      },
+      {
+        name: "Fire escape!",
+
+      },
+      {
+        name: "My cat's stuck in a tree- help!",
+
+      },
+      {
+        name: "So you wanna be a pro wrestler?",
+
+      },
+      {
+        name: "Home Alone X",
+
+      },
+      {
+        name: "Jungle survival",
+
+      },
+      {
+        name: "Where did my passport go?",
+
+      },
+      {
+        name: "This breif case isn't mine, I swear!",
+
+      },
+      {
+        name: "How did you get in?",
+
+      },
+      {
+        name: "Creepy Crawlies",
+
+      },
+      {
+        name: "Spelunking",
+
+      },
+      {
+        name: "Perilous Climbing",
+
+      },
+      {
+        name: "River Run",
+
+      },
+      {
+        name: "A walk gone wrong",
+
+      },
+      {
+        name: "Disaster Production",
+
+      },
+    ]
+  },
+  {
+    name: "Cool Company Games",
+    logo: "logo_url",
+    background: "company_specific_img_url",
+    games: [
+      {
+        name: "Dungeon Crawl",
+
+      },
+      {
+        name: "Diving Adventure",
+
+      },
+      {
+        name: "Fire escape!",
+
+      },
+      {
+        name: "My cat's stuck in a tree- help!",
+
+      },
+      {
+        name: "So you wanna be a pro wrestler?",
+
+      },
+      {
+        name: "Home Alone X",
+
+      },
+      {
+        name: "Jungle survival",
+
+      },
+      {
+        name: "Where did my passport go?",
+
+      },
+      {
+        name: "This breif case isn't mine, I swear!",
+
+      },
+      {
+        name: "How did you get in?",
+
+      },
+      {
+        name: "Creepy Crawlies",
+
+      },
+      {
+        name: "Spelunking",
+
+      },
+      {
+        name: "Perilous Climbing",
+
+      },
+      {
+        name: "River Run",
+
+      },
+      {
+        name: "A walk gone wrong",
+
+      },
+      {
+        name: "Disaster Production",
+
+      },
+    ]
+  },
+  {
+    name: "Mason's wacky adventures",
+    logo: "logo_url",
+    background: "company_specific_img_url",
+    games: [
+      {
+        name: "Dungeon Crawl",
+
+      },
+      {
+        name: "Diving Adventure",
+
+      },
+      {
+        name: "Fire escape!",
+
+      },
+      {
+        name: "My cat's stuck in a tree- help!",
+
+      },
+      {
+        name: "So you wanna be a pro wrestler?",
+
+      },
+      {
+        name: "Home Alone X",
+
+      },
+      {
+        name: "Jungle survival",
+
+      },
+      {
+        name: "Where did my passport go?",
+
+      },
+      {
+        name: "This breif case isn't mine, I swear!",
+
+      },
+      {
+        name: "How did you get in?",
+
+      },
+      {
+        name: "Creepy Crawlies",
+
+      },
+      {
+        name: "Spelunking",
+
+      },
+      {
+        name: "Perilous Climbing",
+
+      },
+      {
+        name: "River Run",
+
+      },
+      {
+        name: "A walk gone wrong",
+
+      },
+      {
+        name: "Disaster Production",
+
+      },
+    ]
+  },
+]
