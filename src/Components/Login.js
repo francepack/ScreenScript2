74dba3f30fd97139b3d4d1abe23645b5ca8a3f34
Mason France <masonrf1@gmail.com>
2020-06-30 10:32:58 -0600

    Add state ideas and list template

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import gameSets from './assets/sampledata.js';
 
-function Login() {
-  return (
-    <div className="login">
-      
-    </div>
-  );
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "",
+      password: "",
+      groups: [],
+    };
+  }
+  
+  render() {
+    return (
+      <div className="login">
+        <h2>Hi there- what group are you logging in with?</h2>
+        <div class="listbox-area">
+          <div class="left-area">
+            <span>
+              Groups:
+            </span>
+            <ul id="groups-list"
+                tabindex="0"
+                role="listbox"
+                aria-labelledby="ss_elem">
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
