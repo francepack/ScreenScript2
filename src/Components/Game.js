c43936584c5c4844a52073e444d1db2f8faef37a
Mason France <masonrf1@gmail.com>
2021-03-18 15:09:14 -0600

    Add start of Game component

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Images from './Images';
 import Screenshare from './Screenshare';
 import Script from './Script';
 import ScriptSelector from './ScriptSelector';
 
-function Game() {
-  return (
-    <div className="game">
-      <div className="screenshare-container">
-        <Screenshare />
+class Game() extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div className="game">
+        <div className="screenshare-container">
+          <Screenshare />
+        </div>
+        <div className="images-container">
+          <Images />
+        </div>
+        <div className="script-container">
+          <Script />
+        </div>
+        <div className="scriptselector-container">
+          <ScriptSelector />
+        </div>
       </div>
-      <div className="images-container">
-        <Images />
-      </div>
-      <div className="script-container">
-        <Script />
-      </div>
-      <div className="scriptselector-container">
-        <ScriptSelector />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Game;
